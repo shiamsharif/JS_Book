8b967005da32def1814b50f9ee5f76a3014bb1f8
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-02 17:13:53 +0600

    ADD: String

---
+++ String/string.js
@@ -0,0 +1,20 @@
+const namee = "shiam sharif";
+console.log(namee);
+
+const address = "jashore";
+console.log(address);
+
+const University = "DU";
+console.log(University);
+
+const fan = "SRK";
+console.log(fan);
+
+const movieOfTheYear = "NaN";
+console.log(movieOfTheYear);
+
+const hobby = "Reading books";
+console.log(hobby);
+
+const color = "Black Blue White Ash";
+console.log(color);
