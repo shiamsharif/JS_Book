673795acbb494bae660ed0e27b0bdbb3eedb8618
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-02 16:54:40 +0600

    ADD: Variable

---
+++ Variable/variable.js
@@ -0,0 +1,15 @@
+const DOB = 2001;
+const sibling = 2;
+const students = 24;
+const marks = 90;
+const rooms = 2;
+const studytime = 12;
+const total = 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11;
+
+console.log(DOB);
+console.log(sibling);
+console.log(students);
+console.log(marks);
+console.log(rooms);
+console.log(studytime);
+console.log(total);
