1806cd6f149b8925d9a7743cc0fa8522d223c8fc
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-02 17:04:05 +0600

    ADD: Console log

---
+++ ConsoleLog/console_log.js
@@ -0,0 +1,20 @@
+const age = 24;
+console.log(age);
+
+const BooksToRead = 13;
+console.log(BooksToRead);
+
+const WakeUpTime = 9;
+console.log(WakeUpTime);
+
+const WestedTime = 3;
+console.log(WestedTime);
+
+const borrow = 100;
+console.log(borrow);
+
+const goals = 11;
+console.log(goals);
+
+const pillow = 3;
+console.log(pillow);
