d958bfeab7183c52e0a7331f312d7644d2c528fe
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-08 20:27:05 +0600

    ADD: Comment

---
+++ Comment/comment.js
@@ -0,0 +1,32 @@
+let score; //Cricket Score
+
+let radius;
+// pi * r * r
+// pi = 3.1416
+// r = 5
+
+let dress;
+// dress: pant, jama
+/*
+amar jama kapor kinte khub ekta bhalo lage nah
+but boi kinte khub bhalo lage
+and ami onk boi pori
+*/
+
+let giftMoney;
+// jani nah kicu
+
+let studyTime;
+// bangla , english, math etc.
+
+let sendMoney;
+/*
+send money = 1000tk
+fee 14.90tk
+*/
+
+/*
+product price : 1500
+dalivary charge : 60
+*/
+let pay;
