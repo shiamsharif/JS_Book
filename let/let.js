ea324b91cfa05812b637e9c45a18b7a5984a48f0
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-08 19:57:21 +0600

    ADD: Let

---
+++ let/let.js
@@ -0,0 +1,29 @@
+let age = 20;
+console.log(age);
+age = 25;
+console.log(age);
+
+let PocketMoney = 500;
+PocketMoney = 150;
+console.log(PocketMoney);
+
+let studyTime = 4;
+studyTime = 8;
+console.log(studyTime);
+
+let friendsCount = 3;
+friendsCount = 1;
+console.log(friendsCount);
+
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+let chairCount = 40;
+chairCount = chairCount - 2;
+chairCount = chairCount - 6;
+console.log(chairCount);
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
