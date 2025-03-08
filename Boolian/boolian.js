e0800449db1821055d1e8dcd88addb314f52211f
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-08 17:07:11 +0600

    ADD: Boolian

---
+++ Boolian/boolian.js
@@ -0,0 +1,22 @@
+isHappy = true;
+console.log(isHappy);
+
+crushMessage = true;
+console.log(crushMessage);
+
+celebrating = true;
+console.log(celebrating);
+
+isHappy = true;
+hasMoney = false;
+isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+isStudyDone = false;
+console.log(isStudyDone);
+
+isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+isDinnerDone = true;
+console.log(isDinnerDone);
