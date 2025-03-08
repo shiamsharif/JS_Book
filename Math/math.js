0be0b7b76f391b29ece6854841b18df9775307cd
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-08 17:26:02 +0600

    ADD: Math

---
+++ Math/math.js
@@ -0,0 +1,41 @@
+const money = 100;
+const expance = 40;
+console.log(money - expance);
+
+const pencil = 10;
+const pancilUse = 2;
+console.log(pencil / pancilUse);
+
+const taka = 60;
+const eachExpance = 15;
+console.log(taka / eachExpance);
+
+const x = "Hello";
+const y = "World";
+console.log(x + y);
+
+const chal = 153;
+const daal = 261;
+console.log(chal + daal);
+
+const a = 500;
+const b = 75;
+console.log(a / b, a % b);
+
+const a1 = 8;
+const b1 = 3;
+console.log(a1 / b1, a1 % b1);
+
+const a2 = 50;
+const b2 = 9;
+console.log(a2 % b2);
+
+const a3 = "Bangla";
+const b3 = "desh";
+console.log(a3 + b3);
+
+const canEat = 4;
+const totalRice = 480;
+const month = 30;
+const monthlyEat = 4 * 30;
+console.log(totalRice / canEat, totalRice / monthlyEat);
