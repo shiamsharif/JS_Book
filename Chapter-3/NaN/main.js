0bae950cfc220f44e77accb02d756970740e92da
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-14 22:37:41 +0600

    Formation

---
+++ Chapter-3/NaN/main.js
@@ -0,0 +1,33 @@
+const num1 = "20";
+const num2 = parseInt(num1) + 10;
+console.log(num2);
+
+const num3 = 3.1416;
+const num4 = parseInt(num3);
+console.log(num4);
+
+const num5 = "pramikBoy";
+const num6 = parseInt(num5);
+console.log(num6);
+
+const a = 3.456;
+const b = 2.789;
+const sum = a + b;
+const sumNum = parseFloat(sum.toFixed(2));
+console.log(sumNum);
+
+const a1 = 56.78;
+const b1 = 12.34;
+const sum1 = a1 + b1;
+const sumNum1 = parseInt(sum1);
+console.log(sumNum1);
+
+const a2 = 10.5678;
+const b2 = 5.4321;
+const sum2 = a2 + b2;
+const sumNum2 = parseFloat(sum2.toFixed(1));
+console.log(sumNum2);
+
+const a3 = 42.45689754;
+const sumNum3 = a3.toFixed(3);
+console.log(sumNum3);
