4d554e9976d54bbfd4bdb815b8901a0b2b0ba702
shiamsharif <shiam.sharif.07@gmail.com>
2025-03-14 20:24:45 +0600

    ADD: Variable Name

---
+++ VaribleName/naming.js
@@ -0,0 +1,24 @@
+const myFavouritFood = "biriyani";
+
+const city = "Chittagong";
+const City = "Dhaka";
+console.log(city);
+console.log(City);
+
+const userName = "Johan";
+const username = "Jovan";
+console.log(userName);
+console.log(username);
+
+const homeAddress = "Jashore";
+console.log(homeAddress);
+
+const super = 16;
+console.log(super);
+
+const class = 12;
+console.log(class); 
+
+const 2ndPosition = 65;
+console.log(2ndPosition); 
+// variable name start with a-z or A-Z, $,_ ... not number or any keywords 
